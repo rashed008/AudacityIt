2137570f22b5330e267345721506dd0bfef9e065
Rashed Parvez <rpervez02@gmail.com>
2019-06-15 16:15:07 +0600

    Login features are completed

---
+++ audacityIt/audacityIt/Model/SignInModel.swift
@@ -0,0 +1,9 @@
+//
+//  SignInModel.swift
+//  audacityIt
+//
+//  Created by RASHED on 6/15/19.
+//  Copyright © 2019 rashed. All rights reserved.
+//
+
+import Foundation
---
+++ audacityIt/audacityIt/Model/SignUpModel.swift
@@ -0,0 +1,9 @@
+//
+//  SignUpModel.swift
+//  audacityIt
+//
+//  Created by RASHED on 6/15/19.
+//  Copyright © 2019 rashed. All rights reserved.
+//
+
+import Foundation
